f0219811587f1c2e06ea64aa9eb8dbcf1c5d138b
Shivangi <dubeyshivangi1910@gmail.com>
2020-12-12 15:01:15 +0530

    first commit

--- myshop/settings.py
+++ myshop/settings.py
@@ -120,9 +120,13 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 STATIC_URL = '/static/'
 MEDIA_URL	=	'/media/'
 MEDIA_ROOT	=	os.path.join(BASE_DIR,	'media/')
 
 CART_SESSION_ID	=	'cart'
+
+
+django_heroku.settings(locals())
